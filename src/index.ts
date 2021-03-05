95af0f5e8a2457aa80bf10d0daf8a4a1fc869476
Devin Spikowski <vegeta897@gmail.com>
2021-03-05 08:58:56 -0500

    Add pixi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { RNG } from 'rot-js'
+import { Application } from 'pixi.js'
+
+const { view } = new Application({ width: 800, height: 600 })
+view.id = 'viewport'
+view.style.display = 'block'
+view.style.margin = '0 auto'
+document.body.appendChild(view)
 
 console.log(RNG.getUniform())
 console.log(RNG.getNormal())
